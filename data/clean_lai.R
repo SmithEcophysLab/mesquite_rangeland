b6f6d38e73961a9f1fd167d6f190bb6bada0c0dc
SmithEcophysLab <nick.gregory.smith@gmail.com>
2019-02-14 09:09:45 -0600

    cleaned LAI data

---
+++ data/clean_lai.R
@@ -0,0 +1,36 @@
+# Clean mesquite LAI data
+## script to clean the LAI data for the mesquite comparison
+
+## load packages
+library(tidyverse)
+library(reshape2)
+
+## read in data
+lai_raw = read.csv('clean_lai/lai_output.csv')
+head(lai_raw)
+nrow(lai_raw)
+
+## will need to create a data frame with date, plot, and LAI
+
+## date
+lai_date = rep(as.Date('2018-09-22'), times = nrow(lai_raw)) # for all 40 plots
+
+## add remarks to LAI data (to indicate plot #)
+lai_remarks = read.csv('clean_lai/lai_remarks.csv')
+
+lai_raw$remark = 'NA'
+for (i in 1:length(lai_raw$B_Obs)){
+  
+  lai_raw$remark[lai_raw$B_Obs == lai_raw$B_Obs[i]] <- as.character(lai_remarks$Remark[lai_remarks$B_Obs == lai_raw$B_Obs[i]])
+  
+}
+
+lai_plot = as.numeric(sapply(strsplit(lai_raw$remark, 'P'), '[', 2))
+
+## combine it all together
+lai_clean = data.frame(date = lai_date, plot = lai_plot, lai = lai_raw$LAI)
+
+## check and write out
+head(lai_clean)
+tail(lai_clean)
+# write.csv(lai_clean, 'lai_clean.csv')
